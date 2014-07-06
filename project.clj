7948a2b2641f6c3427dffe79fcfe19b828ee315e
Uday Verma <uday.karan@gmail.com>
2014-07-06 18:42:26 -0500

    More cleanup and cljs only version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject cljs-firebase "0.0.1"
-  :description "Convenience library for using Firebase with Clojure and Clojurescript"
+  :description "Convenience library for using Firebase Clojurescript"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :author "verma"
@@ -9,29 +9,12 @@
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]]
   :deploy-repositories [["releases" :clojars]]
   :cljsbuild {:builds []}
-  :plugins [[com.keminglabs/cljx "0.4.0"]]
-
-  :aliases {"cljs-build" ["do" "cljx," "with-profile" "cljs" "cljsbuild" "once" "dev"]
-            "clj-test" ["do" "cljx," "with-profile" "clj" "test"]}
-
-  :cljx {:builds [{:source-paths ["src-cljx"]
-                   :output-path "target/gen/clj"
-                   :rules :clj}
-                  {:source-paths ["src-cljx"]
-                   :output-path "target/gen/cljs"
-                   :rules :cljs}]}
 
 
-  :profiles {:cljs {:dependencies []
-                    :plugins [[lein-cljsbuild "1.0.3"]]
-                    :hooks [leiningen.cljsbuild]
-                    :cljsbuild {:builds [{:id "dev"
-                                          :source-paths ["target/gen/cljs"]
-                                          :compiler {:output-to "target/main.js"
-                                                     :optimizations :whitespace
-                                                     :pretty-print true}}]}
-                    :source-paths ["target/gen/cljs"]}
-             :clj {:dependencies []
-                   :source-paths ["target/gen/clj"]
-                   :test-paths ["test"]
-                   :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}}})
+  :plugins [[lein-cljsbuild "1.0.3"]]
+  :hooks [leiningen.cljsbuild]
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src"]
+                        :compiler {:output-to "target/main.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
---
+++ src/firebase/core.cljs
@@ -0,0 +1,61 @@
+(ns firebase.core
+  (:require [cljs.core.async :refer [<! >! chan]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
+
+(defn- clj-val [v]
+  (js->clj (.val v) :keywordize-keys true))
+
+;; Make a firebase object ouf the given URL
+;;
+(defn root [url]
+  "Makes a root reference for firebase"
+  (js/Firebase. url))
+
+;; A utility function to traverse through korks and get ref to a child object
+;; [:hello :world :bye ] refers to hello.world.bye
+;;
+(defn walk-root [root korks]
+  "Takes korks and reduces it to a root on which we can perform direct actions"
+  (cond
+    (sequential? korks) (reduce #(.child %1 (name %2)) root korks)
+    :else (.child root (name korks))))
+
+(defn- fb-call!
+  "Set the value at the given root"
+  ([push-fn root val]
+   (let [as-js (clj->js val)]
+     (push-fn root as-js)))
+
+  ([push-fn root korks val]
+   (fb-call! push-fn (walk-root root korks) val)))
+
+;; A function set the value on a root [korks]
+;;
+(defn set!
+  "Set the value at the given root"
+  ([root val]
+   (fb-call! #(.set %1 %2) root val))
+
+  ([root korks val]
+   (fb-call! #(.set %1 %2) root korks val)))
+
+(defn push!
+  "Set the value at the given root"
+  ([root val]
+   (fb-call! #(.push %1 %2) root val))
+
+  ([root korks val]
+   (fb-call! #(.push %1 %2) root korks val)))
+
+(defn bind
+  "Bind to a certain property under the given root"
+  ([root type korks]
+   (let [c (walk-root root korks)
+         bind-chan (chan)]
+     (.on c (name type) #(go (>! bind-chan (clj-val %1))))
+     bind-chan))
+
+  ([root type korks cb]
+   (let [c (walk-root root korks)]
+     (.on c (name type) #(cb (clj-val %1))))))
+
