1f5a917642b2b4cb68875a56598e53f30d973837
Chris Truter <jeffpalentine@gmail.com>
2015-03-22 13:54:40 +0200

    Fixup initial for namespace
    Your Pani is showing :)

--- test/matchbox/core_test.clj
+++ test/matchbox/core_test.clj
@@ -1,74 +1,71 @@
 (ns matchbox.core-test
   (:require [clojure.test :refer :all]
-            [matchbox.core :as p]))
+            [matchbox.core :as m]))
 
-;; (def firebase-url "https://blazing-fire-1915.firebaseio.com")
 (def firebase-url "https://luminous-torch-5788.firebaseio.com/")
 
 (deftest serialize-test
-  ;; keywords -> strings
+  ;; map keys from keywords -> strings
   (let [orig {:hello "world" :bye "world"}
-        tran (p/serialize orig)]
+        tran (m/serialize orig)]
     (is (contains? tran "hello"))
     (is (not (contains? tran :hello))))
-  ;; unchanged
-  (doseq [x [true [1 2 3 4] 150 "hello"]]
-    (is (= x (p/serialize x)))))
+  ;; values are unchanged
+  (testing "bool, long, float, vector, string, keyword, set, list"
+    (doseq [x [true [1 2 3 4] 150.0 "hello" :a #{1 2 3} '(3 2 1)]]
+      (is (= x (m/serialize x))))))
 
 (deftest hydrate-test
-  ;; strings -> keywords
+  ;; map keys from strings -> keywords
   (let [orig {"hello" "world" "bye" "world"}
-        tran (p/hydrate orig)]
+        tran (m/hydrate orig)]
     (is (contains? tran :hello))
     (is (not (contains? tran "hello"))))
-  ;; unchanged
-  (doseq [x [true [1 2 3 4] 150 "hello"]]
-    (is (= x (p/hydrate x)))))
+  ;; values are unchanged
+  (testing "bool, long, float, vector, string, keyword, set, list"
+    (doseq [x [true [1 2 3 4] 150.0 "hello" :a #{1 2 3} '(3 2 1)]]
+      (is (= x (m/hydrate x))))))
 
 (deftest get-in-test
-  (let [r (p/connect "https://some-app.firebaseio.com/")]
-    (is (= (p/key (p/get-in r [:info :world])) "world"))
-    (is (= (p/key (p/parent (p/get-in r [:info :world]))) "info"))
-    (is (= (p/key (p/get-in r :age)) "age"))))
+  (let [r (m/connect "https://some-app.firebaseio.com/")]
+    (is (= (m/key (m/get-in r [:info :world])) "world"))
+    (is (= (m/key (m/parent (m/get-in r [:info :world]))) "info"))
+    (is (= (m/key (m/get-in r :age)) "age"))))
 
 (deftest reset-test!
-  (let [path (mapv str [(rand-int 500) (rand-int 500)])
-        ref  (p/get-in (p/connect firebase-url) path)
-        val  (rand-int 1000)
-        p    (promise)]
-    (p/listen-to ref :value #(deliver p %))
-    (p/reset! ref val)
-    (is (= [(last path) val] @p))))
+  (let [ref (rand-ref)
+        val (rand-int 1000)
+        p   (promise)]
+    (m/listen-to ref :value #(deliver p %))
+    (m/reset! ref val)
+    (is (= [(m/key ref) val] @p))))
 
 (deftest deref-test
   (let [path (mapv str [(rand-int 500) (rand-int 500)])
-        ref  (p/get-in (p/connect firebase-url) path)
+        ref  (m/get-in (m/connect firebase-url) path)
         val  (rand-int 1000)
         p    (promise)]
-    (p/deref ref #(deliver p %))
-    (p/reset! ref val)
+    (m/deref ref #(deliver p %))
+    (m/reset! ref val)
     (is (= [(last path) val] @p))))
 
 (deftest conj-test!
   (let [path (mapv str [(rand-int 500) (rand-int 500)])
-        ref  (p/get-in (p/connect firebase-url) path)
+        ref  (m/get-in (m/connect firebase-url) path)
         val  (rand-int 1000)
         p1   (promise)
         p2   (promise)]
-    (p/listen-to ref :child-added #(deliver p1 %))
-    (p/conj! ref val)
+    (m/listen-to ref :child-added #(deliver p1 %))
+    (m/conj! ref val)
     (is (= val (last @p1)))
-    ;; FIXME times out.. probably also a persistence issue
-    ;;
-    ;; (p/listen-to ref (first @p1) :value #(deliver p2 %))
-    ;; (is (= @p1 @p2))
-    ))
+    (m/listen-to ref (first @p1) :value #(deliver p2 %))
+    (is (= @p1 @p2))))
 
 (deftest swap!-test
   (let [path (mapv str [(rand-int 500) (rand-int 500)])
-        ref  (p/get-in (p/connect firebase-url) path)
+        ref  (m/get-in (m/connect firebase-url) path)
         val  (rand-int 1000)
         p    (promise)]
-    (p/listen-to ref :value #(deliver p %))
-    (p/swap! ref vector val 512)
+    (m/listen-to ref :value #(deliver p %))
+    (m/swap! ref vector val 512)
     (is (= [(last path) [nil val 512]] @p))))
