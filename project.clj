fb6968af2510bd6305ed0987de96a7db905dc4d9
Chris Truter <jeffpalentine@gmail.com>
2015-03-30 12:36:24 +0200

    Abort alpha (`lein release alpha` released 0.0.6..)
    Obviously did not use `lein release` properly, have to take more care next time.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matchbox "0.0.6-alpha1"
+(defproject matchbox "0.0.6"
   :description "Firebase bindings for Clojure(Script)"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
